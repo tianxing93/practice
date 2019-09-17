18ea1331a757a0dc4d511f55c0509ea88ab52de7
Liuliyisui <m18628156073@163.com>
2019-09-17 20:51:58 +0800

    第二次提交

--- practice-9-9/src/test/java/com/qf/AppTest.java
+++ practice-9-9/src/test/java/com/qf/AppTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -11,7 +13,7 @@ public class AppTest
 {
 
     @Test
-    public static void main(String[] args) {
+    public void test() {
         Scanner scan = new Scanner(System.in);
         System.out.println("请输入整数N:");
         int n = scan.nextInt();
@@ -31,5 +33,23 @@ public class AppTest
             }
         }
         System.out.println(msg + "=" +sum);
+    }
+
+    @Test
+    public  void test1(){
+        List<String> list = new ArrayList<>();
+        for(char i = 'A';i <= 'F' ;i++){
+            list.add(""+i);
+        }
+        System.out.println("六个人的编号为：");
+        for (int i = 0; i <list.size() ; i++) {
+            System.out.print(list.get(i)+" ");
+        }
+        System.out.println();
+        System.out.println("中奖者为：");
+        for(int k = 0;k < 3;k++){
+            int d = (int)(Math.random()*(6-k));
+            System.out.print(list.remove(d)+" ");
+        }
     }
 }
